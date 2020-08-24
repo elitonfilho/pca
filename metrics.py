beeaefb7af2596bd9d7cd567edfe180adec3a4a3
Eliton <elitonfilho@gmail.com>
2020-08-24 12:27:09 -0300

    Updates on metrics and test

--- metrics.py
+++ metrics.py
@@ -2,13 +2,13 @@ import numpy as np
 
 class Metrics():
 
-    def __init__(self, nbands=6, nclass=18):
+    def __init__(self, nbands=6, nclass=19):
         self.nbands = nbands
         self.nclass = nclass
-        self.confusion_matrix = np.zeros((n_class, nclass))
+        self.confusion_matrix = np.zeros((nclass, nclass))
 
-    def metrics(self,l_pred, l_true):
-        l_pred, l_true = self.use_mask(l_pred, l_true)
+    def metrics(self,mask, l_pred, l_true):
+        l_pred, l_true = self.use_mask(mask ,l_pred, l_true)
         hist = self.confusion_matrix
         acc = np.diag(hist).sum() / hist.sum()
         acc_cls = np.diag(hist) / hist.sum(axis=1)
@@ -17,7 +17,7 @@ class Metrics():
         mean_iu = np.nanmean(iu)
         freq = hist.sum(axis=1) / hist.sum()
         fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
-        cls_iu = dict(zip(range(self.n_class), iu))
+        cls_iu = dict(zip(range(self.nclass), iu))
         return (
             {
                 "Overall Acc: \t": acc,
@@ -35,33 +35,10 @@ class Metrics():
 
     def hist(self, l_pred, l_true):
         hist = np.bincount(
-            self.nclass * l_pred.astype(int) + label_pred, minlength=self.n_class ** 2
+            self.nclass * l_true.astype(int) + l_pred, minlength=self.nclass ** 2
         ).reshape(self.nclass, self.nclass)
         return hist
 
     def update(self, l_pred, l_true):
         for lp, lt in zip(l_pred, l_true):
             self.confusion_matrix += self.hist(lp.flatten(), lt.flatten())
-
-    def mapClasses(self, l):
-        pred_to_true = {
-            0:,
-            1:,
-            2:,
-            3:
-            4:
-            5:
-            6:
-            7:
-            8:
-            9:
-            10:
-            11:
-            12:
-            13:
-            14: 12,
-            15: 0,
-            16: 0,
-            17:
-            18: 
-        }
--- test.py
+++ test.py
@@ -2,12 +2,13 @@ import torch
 from torchvision.transforms import Compose, ToTensor, Normalize
 from model import *
 from utils import *
+from metrics import Metrics
 import matplotlib.pyplot as plt
 import cv2
 
 if __name__ == "__main__":
     unet_resnet = UNetResNet(num_classes=19, in_channels=6)
-    model_path= './unet-final'
+    model_path= './unet6'
     pretrained_model = torch.load(model_path)
     for name, tensor in pretrained_model.items():
         unet_resnet.state_dict()[name].copy_(tensor)
@@ -20,48 +21,32 @@ if __name__ == "__main__":
     softmax2d = torch.nn.Softmax2d()
 
     count = 0
+    metric = Metrics()
     for img, mask , label in val_loader:
-    # img = cv2.imread('./data/val/hr/2953-3-SO_300_HR.png')
-    # img = np.expand_dims(img, axis=0)
-    # norm_img = np.zeros((256,256))
-    # img = cv2.normalize(img,  norm_img, 0, 255, cv2.NORM_MINMAX)
-    # img = img.transpose(0, 3, 1, 2)
-    # img = torch.FloatTensor(img)
-    # img = img.cuda()
 
         with torch.no_grad():
             pred = unet_resnet(img.float())
             pred = softmax2d(pred)
             pred = pred.argmax(1)
-        pred = pred.data.cpu().numpy()
 
-        # for i in range(19):
-        #     plt.subplot(1,19,i+1)
-        #     plt.xticks([])
-        #     plt.yticks([])
-        #     plt.imshow(pred[:,i,:,:].squeeze(), cmap='gray')
+            pred = pred.data.cpu().numpy()
+            mask = mask.data.cpu().numpy()
+            label = label.data.cpu().numpy()
 
-        bz = 5
+            metric.update(pred, label)
+            print(metric.metrics(mask, pred, label))
 
-        fig, ax = plt.subplots(3,5)
-        for i in range(bz):
-            ax[0,i].imshow(img[i,0,:,:].squeeze())
-            ax[1,i].imshow(label[i].squeeze())
-            # ax[1,i].imshow(mask[i].squeeze())
-            ax[2,i].imshow(pred[i], cmap='gray')
+        # bz = 5
+        # fig, ax = plt.subplots(3,5)
+        # for i in range(bz):
+        #     ax[0,i].imshow(img[i,0,:,:].squeeze())
+        #     ax[1,i].imshow(label[i].squeeze())
+        #     # ax[1,i].imshow(mask[i].squeeze())
+        #     ax[2,i].imshow(pred[i], cmap='gray')
 
         # plt.show()
 
-        # fig, ax = plt.subplots(1,3)
-        # print(img.shape)
-        # ax[0].imshow(img.squeeze()[0,:,:])
-        # ax[1].imshow(mask.squeeze())
-        # ax[2].imshow(pred, cmap='gray')
 
-        plt.show()
-
-        # cv2.imwrite(f'output/mask{count}.png', pred)
-        # count+= 1
     
     # with torch.no_grad():
     #     correct = 0
